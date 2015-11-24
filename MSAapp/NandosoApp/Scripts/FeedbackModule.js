fdcc1fb2cb72ceaa44ab1bf3761d52c73fc6b5fd
Angel <a.tuyay.castro@gmail.com>
2015-11-25 12:51:56 +1300

    Updated Homepage UI

--- MSAapp/NandosoApp/Scripts/FeedbackModule.js
+++ MSAapp/NandosoApp/Scripts/FeedbackModule.js
@@ -1,14 +1,22 @@
 ﻿var FeedbackModule = (function () {
     // Return anything that you want to expose outside the closure
     return {
-        addFeedback: function (feedback) {
+        addFeedbackPost: function (topic) {
+            var jsonToPost = '{'
+                    + '"FeedbackPostID" : "0",'
+                    + '"topic" : '
+                    + topic
+                    + ' }';
+
             $.ajax({
                 type: "POST",
+                data: jsonToPost,
                 dataType: "json",
+                contentType: "application/json; charset=utf-8",
                 url: "https://acasnandoso.azurewebsites.net/api/Feedback",
                 success: function (data) {
                     console.log(data);
-                    //callback(data);
+                    alert("Feedback post successfully created.");
                 }
             });
         }
